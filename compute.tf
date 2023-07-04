b0cc9d4c9f0ecf2dafb9187688a4039e742f300b
Sylvain K <sylvain.kamdem@gmail.com>
2023-07-04 10:36:39 +0100

    completion of provisioner project

--- compute.tf
+++ compute.tf
@@ -1,16 +1,16 @@
 # compute instances of aws provider
 
-data "aws_key_pair" "keypair" {
-  key_name = "some_key"
-  filter {
-    name   = "key-name"
-    values = ["some_key"]
-  }
-}
 # data "aws_key_pair" "keypair" {
-#   key_name   = "some_key"
-#   public_key = var.public_key
+#   key_name = "some_key"
+#   filter {
+#     name   = "key-name"
+#     values = ["some_key"]
+#   }
 # }
+resource "aws_key_pair" "keypair" {
+  key_name   = "my_nginx_key"
+  public_key = var.public_key
+}
 
 data "aws_ami" "ami" {
   most_recent = true
@@ -27,40 +27,15 @@ resource "aws_instance" "nginx_instance" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.instance_sg.id]
-  key_name               = data.aws_key_pair.keypair.key_name
+  key_name               = resource.aws_key_pair.keypair.key_name
 
   tags = {
-    Name        = "temp_server" //"${local.resources_tag}"
+    Name        = "${local.resources_tag}"
     "Terraform" = "Yes"
   }
 
-  # The connection block tells our provisioner how to
-  # communicate with the resource (instance)
-  #   connection {
-  #     host        = aws_instance.nginx_instance.public_ip
-  #     type        = var.connection_protocol
-  #     user        = var.username
-  #     private_key = file(var.private_key_path)
-  #     #agent       = "true"
-  #   }
-
-  # We run a remote provisioner on the instance after creating it.
-  # In this case, we install nginx and start it. By default,
-  # this should be on port 80
-
-  #   provisioner "remote-exec" {
-  #     inline = [
-  #       "sudo amazon-linux-extras enable nginx${var.nginx_version}",
-  #       "sudo yum -y install nginx",
-  #       "sudo chmod 777 /usr/share/nginx/html/index.html",
-  #       "echo \"Hello from our nginx server in AWS\" > /usr/share/nginx/html/index.html",
-  #       "sudo systemctl start nginx",
-  #     ]
-  #   }
-}
-
-resource "null_resource" "test_ssh_from_windows" {
-
+  #   The connection block tells our provisioner how to
+  #   communicate with the instance
   connection {
     host        = aws_instance.nginx_instance.public_ip
     type        = var.connection_protocol
@@ -68,6 +43,10 @@ resource "null_resource" "test_ssh_from_windows" {
     private_key = file(var.private_key_path)
     agent       = "false" //use the local SSH agent for authentication
   }
+
+  #   We run a remote provisioner on the instance after creating it.
+  #   In this case, we install nginx and start it. By default,
+  #   this should be on port 80
 
   provisioner "remote-exec" {
     inline = [
@@ -79,3 +58,28 @@ resource "null_resource" "test_ssh_from_windows" {
     ]
   }
 }
+
+# resource "null_resource" "test_ssh_from_windows" {
+
+#   connection {
+#     host        = aws_instance.nginx_instance.public_ip
+#     type        = var.connection_protocol
+#     user        = var.username
+#     private_key = file(var.private_key_path)
+#     agent       = "false" //use the local SSH agent for authentication
+#   }
+
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo amazon-linux-extras enable nginx1",
+#       "sudo yum -y install nginx",
+#       "sudo chmod 777 /usr/share/nginx/html/index.html",
+#       "echo \"Hello from our nginx server in AWS\" > /usr/share/nginx/html/index.html",
+#       "sudo systemctl start nginx",
+#     ]
+#   }
+
+#   provisioner "local-exec" {
+#     command = "curl http://${aws_instance.nginx_instance.public_ip}"
+#   }
+# }
